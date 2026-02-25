284867104f33f5a62682b62a9d26bbd4574a2005
shanew <shaneweerasuriya@gmail.com>
2026-02-25 09:34:40 -0700

    feat: wire GHCLIService into MenuBarExtra app entry point

--- GitSwitcher/App/GitSwitcherApp.swift
+++ GitSwitcher/App/GitSwitcherApp.swift
@@ -2,9 +2,17 @@ import SwiftUI
 
 @main
 struct GitSwitcherApp: App {
+    @StateObject private var service = GHCLIService()
+
     var body: some Scene {
-        MenuBarExtra("Git", systemImage: "person.crop.circle") {
+        MenuBarExtra {
             ContentView()
+                .environmentObject(service)
+        } label: {
+            Label(
+                service.activeAccount?.login ?? "Git",
+                systemImage: "person.crop.circle"
+            )
         }
         .menuBarExtraStyle(.menu)
     }
